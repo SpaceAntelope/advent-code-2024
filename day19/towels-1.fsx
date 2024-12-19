500ba741c58e6b1abb695f638eb79ec678c47c9e
Ares Lazarus <cernunos_kav@msn.com>
2024-12-19 22:11:44 +0200

    19 wip

---
+++ day19/towels-1.fsx
@@ -0,0 +1,57 @@
+open System
+open System.IO
+
+#load "../global.fsx"
+open System.Collections.Generic
+
+let parse path=
+    let lines =
+        path
+        |> File.ReadAllLines
+
+    let patterns = lines.[0].Split(',') |> Array.map _.Trim()
+    let designs = lines |> Array.skip 2
+
+    patterns, designs
+
+let mutable count = 0
+let synthesize (genepool: string[]) (target: string) =
+    let relevantGenes = genepool |> Array.filter (fun gene -> target.Contains gene)
+    let rec search index (genome : string list) : string list option  =        
+        if target = (genome |> String.concat "")
+        then Some genome                
+        else if index >= target.Length
+        then None
+        else
+            count <- count + 1
+
+            relevantGenes 
+            |> Seq.filter (fun gene -> 
+                gene.[0] = target.[index] 
+                && target.Length - index >= gene.Length
+                && gene = target.Substring(index,gene.Length))
+            |> Seq.tryPick (fun gene -> search (index + gene.Length) (genome@[gene]) )
+
+    search 0 []
+
+parse "./input.example"
+|> fun (patterns,designs) ->
+    designs    
+    |> Array.map ((synthesize patterns)>>Option.isSome)
+    |> Global.shouldBe [|true;true;true;true;false;true;true;false|]
+
+
+parse "./input.actual"
+|> fun (patterns,designs) ->
+    designs    
+    |> Array.choose (synthesize patterns)
+    |> Array.length
+    |> printfn "There are %d possible designs."
+
+printfn "%d" count
+
+// parse "./input.actual"
+// |> fun (patterns,designs) ->
+//     designs    
+//     |> Array.filter ((synthesize patterns)>>Option.isNone)
+//     |> fun lines -> File.WriteAllLines("./unsolvable.designs", lines)
