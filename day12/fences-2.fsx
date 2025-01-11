64d0359ce103aff8be08a2ece48a49bef59bcb4c
Ares Lazarus <cernunos_kav@msn.com>
2025-01-11 22:22:13 +0200

    12-2 done

--- day12/fences-2.fsx
+++ day12/fences-2.fsx
@@ -5,45 +5,6 @@ open System.Collections.Generic
 #load "../global.fsx"
 #load "./common.fsx"
 
-// type Direction = H | V
-
-// let traversePerimeter (matrix : char array2d) (perimeter: (int*int) list) = 
-//     let perimSet = HashSet<int*int>(perimeter)
-//     let visited = HashSet<int*int>()
-//     let perimNeibourhood (row,col) =
-//         [   row-1, col-1; row-1,col; row-1,col+1;
-//             row, col-1; row, col+1;
-//             row+1, col-1; row+1,col; row+1,col+1
-//         ] |> List.filter (fun (r,c ) -> perimSet.Contains(r,c) && (not>>visited.Contains)(r,c))
-
-//     let rec walk (sideCounter: int) (row,col) =
-//         match visited.Add row,col with
-//         | true -> 
-//             perimNeibourhood (row,col)             
-//             |> function
-//             | r,c when r = row -> walk sideCounter (r,c) 
-//             | r,c when c = col -> walk sideCounter (r,c)  
-//             | r,c when r <> row && c <> col -> walk (sideCounter+1) (r,c)
-//         | false -> 0
-
-//     ()
-    
-
-
-
-let isOutOfBoundsFactory matrix = 
-    let size = Global.matrixSize matrix
-    let isOutOfBounds = Global.isOutOfBounds size
-    fun row col -> isOutOfBounds (row,col)
-
-
-let borderOfRegion matrix region=
-    let isOutOfBounds = isOutOfBoundsFactory matrix
-    region
-    |> Seq.filter (fun (row,col) -> 
-        let n = Common.neighbourhood matrix (row,col)
-        n |> List.exists (fun (r,c) -> isOutOfBounds r c || matrix.[row,col] <> matrix.[r,c]))
-
 type Point = int*int
 type Dir = 
     | Up | Dn | Lt | Rt
@@ -57,12 +18,6 @@ type Dir =
             | diffRow, diffCol when diffRow > 0 && diffCol = 0 -> Up
             | diffRow, diffCol when diffRow < 0 && diffCol = 0 -> Dn
             | diffRow, diffCol  -> failwithf $"Don't how to get from %A{(r1,c1)} to $A{(r2,c2)}"
-        member x.RotateRight =
-            match x with
-            | Up -> Rt
-            | Rt -> Dn
-            | Dn -> Lt
-            | Lt -> Up
 
 type DirectedBorder = {
     Row: int
@@ -71,26 +26,16 @@ type DirectedBorder = {
     with 
         member x.HasDir(dir:Dir) = 
             x.Directions |> List.contains dir
+        override x.ToString() = $"{x.Row},{x.Col} %A{x.Directions}"
 
 let neighbourhood point = 
     let row,col = point
     [row-1,col;row,col+1;row+1,col;row,col-1]
 
-
-let regionBorder matrix region = 
-    let isOutOfBounds = isOutOfBoundsFactory matrix
-    
-    let regionPlot = 
-        region 
-        |> List.head 
-        |> fun (row,col) -> matrix.[row,col]
-        
-    region
-    |> List.map (fun point -> point, neighbourhood point )
-    |> List.filter (fun (pt, n) ->
-        n |> List.exists (fun (row,col) -> isOutOfBounds row col || matrix.[row,col] <> regionPlot))
-    |> List.map fst
-    |> Set.ofList
+let isOutOfBoundsFactory matrix = 
+    let size = Global.matrixSize matrix
+    let isOutOfBounds = Global.isOutOfBounds size
+    fun row col -> isOutOfBounds (row,col)
 
 let regionDirectedBorder matrix region = 
     let isOutOfBounds = isOutOfBoundsFactory matrix
@@ -104,8 +49,6 @@ let regionDirectedBorder matrix region =
     region
     |> List.map (fun point -> point, neighbourhood point )
     |> List.filter (fun (pt, n) ->
-        // n |> List.fold(fun s c -> sprintf "N: %s %A (%c)" s c matrix.[fst c, snd c]) ""
-        // |> printfn "C: %A %c %s" pt matrix.[fst pt, snd pt]
         n |> List.exists isOutOfRegion)
     |> List.map (fun (pt1,n) ->
         pt1, n |> List.filter isOutOfRegion |> List.map (fun pt2 -> Dir.From(pt1,pt2)) )
@@ -114,26 +57,23 @@ let regionDirectedBorder matrix region =
 let sidesOfRegion matrix region =
     let borderSet = regionDirectedBorder matrix region
 
+    // borderSet |> printfn "RDB: %A"
+    let getRelevantGroup grp = 
+        grp 
+        |> List.filter (fun ((_,hasDir),_) -> hasDir) 
+        |> List.map snd
+
     let topSides = 
-        borderSet 
-        |> List.groupBy (fun border -> 
-            border.Row,border.HasDir Up)
+        borderSet |> List.groupBy (fun border -> border.Row,border.HasDir Up) |> getRelevantGroup
     let bottomSides = 
-        borderSet 
-        |> List.groupBy (fun border -> 
-            border.Row,border.HasDir Dn)
+        borderSet |> List.groupBy (fun border -> border.Row,border.HasDir Dn) |> getRelevantGroup
     let leftSides = 
-        borderSet 
-        |> List.groupBy (fun border -> 
-            border.Col,border.HasDir Lt)
+        borderSet |> List.groupBy (fun border -> border.Col,border.HasDir Lt) |> getRelevantGroup
     let rightSides = 
-        borderSet 
-        |> List.groupBy (fun border -> 
-            border.Col,border.HasDir Rt)
+        borderSet |> List.groupBy (fun border -> border.Col,border.HasDir Rt) |> getRelevantGroup
 
     let horizontalSideCount rowSet = 
         rowSet 
-        |> List.map snd 
         |> List.sumBy (fun rowGroup -> 
             rowGroup
             |> List.sortBy _.Col 
@@ -144,12 +84,11 @@ let sidesOfRegion matrix region =
     
     let verticalSideCount rowSet = 
         rowSet 
-        |> List.map snd 
         |> List.sumBy (fun colGroup -> 
             colGroup
             |> List.sortBy _.Row
             |> List.pairwise 
-            |> List.filter (fun (bd1,bd2) -> bd2.Col - bd1.Col <> 1) 
+            |> List.filter (fun (bd1,bd2) -> bd2.Row - bd1.Row <> 1) 
             |> List.length 
             |> (+) 1)
     
@@ -158,171 +97,45 @@ let sidesOfRegion matrix region =
     + verticalSideCount leftSides
     + verticalSideCount rightSides
 
-let showRegions path =
+let calculateFenceCost path =
     let matrix = 
         path
         |> Common.parse
 
     matrix
     |> Common.findRegions
-    |> List.iter (fun region -> 
-        let perim= Common.perimeter matrix (region |> List.ofSeq)
-        Common.printPerim matrix perim
+    |> List.sumBy (fun region -> 
+        // let perim = Common.perimeter matrix (region |> List.ofSeq)
+        // Common.printPerim matrix perim
         
-        regionDirectedBorder matrix (region |> List.ofSeq)
-        |> printfn "RDB: %A"
+        let sides = sidesOfRegion matrix (region |> List.ofSeq)
 
-        sidesOfRegion matrix (region |> List.ofSeq)
-        |> printfn "Found %d sides"
-    )
+        let costOfRegion = sides * region.Count
 
-showRegions "input.test1"
-showRegions "input.test2"
-showRegions "input.example"
-showRegions "input.example2"
-
-// let sidesOfRegion region = 
-//     let isOutOfBounds = isOutOfBoundsFactory matrix
-//     let isInBounds row col = (isOutOfBounds row col) |> not
-//     let border = regionBorder matrix region
-//     let start = border |> Set.minElement
-
-//     let rec walk (current: Point) (previous: Point) (currentDir: Dir) walked =
-//         let row,col = current
-//         let prevRow, prevCol = previous
+        // printfn "Found %d sides for a cost of %i" sides costOfRegion
         
-//         if walked |> Set.contains (row,col,dir) 
-//         then 0
-//         else if isInBounds row col && matrix.[row,col] = matrix.[prevRow,prevCol]
-//         then 
-//             let walked' = walked |> Set.add (row,col,dir)
-//             let score = if currentDir <> lastDir then 1 else 0
-            
-//             score 
-//             + walk (row+1,col) currentDir Dn walked'
-//             + walk (row-1,col) currentDir Up walked'
-//             + walk (row,col+1) currentDir Rt walked'
-//             + walk (row,col-1) currentDir Lt walked'
-//         else 0
-      
-                    
-                        
+        costOfRegion)
 
+"input.test1"
+|> calculateFenceCost 
+|> Global.shouldBe 80
 
-                
+"input.test2"
+|> calculateFenceCost 
+|> Global.shouldBe 436
 
-exit 666
+"input.test3"
+|> calculateFenceCost 
+|> Global.shouldBe 236
 
-let calculateCost path expected= 
-    let matrix = 
-        path
-        |> Common.parse
+"input.example"
+|> calculateFenceCost 
+|> Global.shouldBe 1206
 
+"input.example2"
+|> calculateFenceCost 
+|> Global.shouldBe 368
 
-    printfn "%A" matrix
-    matrix    
-    |> Common.findRegions
-    |> List.map (List.ofSeq)
-    |> List.fold (fun cost region -> 
-        let perimeter = Common.perimeter matrix region
-        let tee msg x = printfn "%s %A" msg x; x
-        let hSides = perimeter |> List.countBy fst |> tee "H: " |> List.length
-        let vSides = perimeter |> List.countBy snd |> tee "V: " |> List.length
-        printfn "region %c \n%A\nperimeter\n%A\nh: %d v: %d\n" (region |> List.head |> fun (r,c) -> matrix.[r,c]) region perimeter hSides vSides
-        cost + region.Length * (hSides*vSides) ) 0
-    |> fun cost ->
-        if expected > 0 then Global.shouldBe expected cost
-        else printfn "Total bulk cost estimated at %d" cost
-
-// calculateCost "./input.test1" 80
-// calculateCost "./input.test2" 236
-// calculateCost "./input.example" 1206
-// calculateCost "./input.example2" 368
-// calculateCost "./input.actual" -1
-
-let tee msg x = printfn "%s %A" msg x; x
-
-// let matrix = Common.parse "./input.test1"
-// matrix |> Global.print
-
-// let perims = 
-//     matrix
-//     |> Common.findRegions
-//     |> List.map (List.ofSeq)
-//     |> List.map (Common.perimeter matrix)
-//     |> List.map (fun per -> 
-//         Common.printPerim matrix per
-//         let (rowCount,colCount) = Global.matrixSize matrix
-        
-
-//         per 
-//         |> List.groupBy fst
-//         |> List.sumBy (fun (rowIndex, rowGroup) -> 
-//             printfn "peri for row %d: %A" rowIndex (rowGroup |> List.sortBy snd)
-//             let sides = 
-//                 rowGroup 
-//                 |> List.sortBy snd
-//                 //|> List.pairwise
-//                 |> List.fold (fun sideCount (row,col) -> 
-                        
-//                         if per |> List.contains (row+1,col) || per |>List.contains (row-1,col)
-//                         then sideCount
-//                         else
-
-//                             let nextFence = 
-//                                 if rowGroup |> List.contains (row,col+1) && col < colCount
-//                                 then Some(row,col+1)
-//                                 else None
-
-//                             printfn "%A -> %A" (row,col) nextFence
-
-//                             match nextFence with
-//                             | None when 
-//                                 (per |> List.contains (row-1,col+1) 
-//                                 && per |> List.contains (row+1,col+1))
-//                                 ||
-//                                 per |> List.contains (row-1,col-1) 
-//                                 && per |> List.contains (row+1,col-1)
-//                                     -> sideCount
-//                             | None -> sideCount + 1
-//                             | Some (nextRow,nextCol) when Math.Abs(nextCol-col) = 1 -> sideCount
-//                             | Some (nextRow,nextCol) when 
-//                                 per |> List.contains (row-1,nextCol) 
-//                                 && per |> List.contains (row+1,nextCol) -> sideCount
-//                             | Some (nextRow,nextCol) when 
-//                                 per |> List.contains (row-1,nextCol-1) 
-//                                 && per |> List.contains (row+1,nextCol-1) -> sideCount                        
-//                             | Some x ->                           
-//                                 sideCount + 1) 0            
-
-//             printfn "Row: Horizontal sides in %A: %d" rowGroup sides
-//             sides)
-//             |> printfn "Total horizontal sides for region: %A"        
-//             )
-//     |> printfn "Total sides: %A"
-
-//         // per 
-//         // |> List.groupBy fst 
-//         // |> List.filter (fun (key,grp) -> 
-//         //     grp
-//         //     |> List.sortBy snd
-//         //     |> List.pairwise 
-//         //     |> List.forall (fun ((r1,c1),(r2,c2)) -> 
-//         //                 //printfn "%d,%d - %d, %d | %d " r1 c1 r2 c2 (r1-r2); 
-//         //                 Math.Abs(c1-c2)=1 ))
-//         // |> List.map snd
-//         // |> List.iter (printfn "h: %A")
-
-//         // per 
-//         // |> List.groupBy snd 
-//         // |> List.filter (fun (key,grp) -> 
-//         //     grp
-//         //     |> List.sortBy fst
-//         //     |> List.pairwise 
-//         //     // forall on empty returns true so we get 1 cell length sides too
-//         //     |> List.forall (fun ((r1,c1),(r2,c2)) -> 
-//         //                 //printfn "%d,%d - %d, %d | %d " r1 c1 r2 c2 (r1-r2); 
-//         //                 Math.Abs(r1-r2)=1 ))
-//         // |> List.map snd
-//         // |> List.iter (printfn "v: %A")
-//         // )
+"input.actual"
+|> calculateFenceCost 
+|> printfn "The new total price of fencing all regions on the map is %i"
